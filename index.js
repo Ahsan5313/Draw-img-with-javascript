3d7217c745bfb9c2443dcd4c53ffc5e048de5ad3
Ahsan5313 <81156921+Ahsan5313@users.noreply.github.com>
2021-04-14 11:47:40 +0600

    add html and javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+function btn(){
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("screm");
+    ctx.drawImage(img,0,0,);
+}
